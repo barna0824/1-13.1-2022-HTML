8d236ae4fb70f4fbeb4b03bc48d9acacaa9493cf
barna0824 <barna0824@gmail.com>
2022-11-29 11:42:19 +0100

    oo

---
+++ Python 5.14/5.14-2.py
@@ -0,0 +1,12 @@
+napok = ["Hétfő","Kedd","Szerda","Csütörtök","Péntek","Szombat","Vasárnap"]
+napIndex = 2
+
+
+for i in range(2,137):
+    if napIndex !=6:
+       napIndex +=1
+    else:
+       napIndex= 0
+
+
+print(f"{137-3} éjszakát voltál ott és {napok[napIndex]}i napon jössz haza.")
---
+++ Python 5.14/5.14-5.py
@@ -0,0 +1,12 @@
+""""
+
+F F F = False
+F F T = True
+F T F = False
+F T T = True
+T F F = False
+T F T = True
+T T F = True
+T T T = True
+
+"""
